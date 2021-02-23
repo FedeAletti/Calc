ff0b59d7d26ecd7b2fb00fbbcc2b1a377372a9d3
Fede Aletti <fedeale2000@gmail.com>
2021-02-23 01:11:31 -0300

    Calculadora con listeners de teclado

--- app.js
+++ app.js
@@ -17,15 +17,118 @@ let p = {
 /*                    OBJETO CON METODOS DE LA CALCULADORA                    */
 /* -------------------------------------------------------------------------- */
 let m = {
-    inicio: function(){
-    
-        for(let i = 0; i < p.teclas.length; i++){
+    inicio: function () {
+
+        for (let i = 0; i < p.teclas.length; i++) {
             p.teclas[i].addEventListener("click", m.oprimirTecla)
         }
     },
 
-    oprimirTecla: function(tecla) {
-        
+    teclado: function () {
+
+        document.addEventListener("keydown", m.oprimir);
+
+    },
+
+    oprimir: function (tecla) {
+
+        console.log(tecla.keyCode);
+
+        if (tecla.keyCode == 48 || tecla.keyCode == 96) {
+            p.accion = "numero";
+            p.digito = 0;
+
+        };
+        if (tecla.keyCode == 49 || tecla.keyCode == 97) {
+            p.accion = "numero";
+            p.digito = 1;
+
+        };
+
+        if (tecla.keyCode == 50 || tecla.keyCode == 98) {
+            p.accion = "numero";
+            p.digito = 2;
+
+        };
+        if (tecla.keyCode == 51 || tecla.keyCode == 99) {
+            p.accion = "numero";
+            p.digito = 3;
+
+        };
+
+
+        if (tecla.keyCode == 52 || tecla.keyCode == 100) {
+            p.accion = "numero";
+            p.digito = 4;
+
+        };
+        if (tecla.keyCode == 53 || tecla.keyCode == 101) {
+            p.accion = "numero";
+            p.digito = 5;
+
+        };
+
+        if (tecla.keyCode == 54 || tecla.keyCode == 102) {
+            p.accion = "numero";
+            p.digito = 6;
+
+        };
+        if (tecla.keyCode == 55 || tecla.keyCode == 103) {
+            p.accion = "numero";
+            p.digito = 7;
+
+        };
+
+        if (tecla.keyCode == 56 || tecla.keyCode == 104) {
+            p.accion = "numero";
+            p.digito = 8;
+
+        };
+        if (tecla.keyCode == 57 || tecla.keyCode == 105) {
+            p.accion = "numero";
+            p.digito = 9;
+
+        };
+
+        if (tecla.keyCode == 187 || tecla.keyCode == 107) {
+            p.accion = "signo";
+            p.digito = "+";
+
+        };
+        if (tecla.keyCode == 189 || tecla.keyCode == 109) {
+            p.accion = "signo";
+            p.digito = "-";
+        };
+        if (tecla.keyCode == 88 || tecla.keyCode == 106) {
+            p.accion = "signo";
+            p.digito = "*";
+        };
+        if (tecla.keyCode == 111) {
+            p.accion = "signo";
+            p.digito = "/";
+
+        };
+
+        if (tecla.keyCode == 190 || tecla.keyCode == 110) {
+            p.accion = "decimal";
+            p.digito = ".";
+        };
+
+        if (tecla.keyCode == 13) {
+            p.accion = "igual";
+        };
+
+        if (tecla.keyCode == 8) {
+            m.borrarCalculadora();
+        };
+
+        m.calculadora(p.accion, p.digito)
+
+
+    },
+
+    oprimirTecla: function (tecla) {
+
         p.accion = tecla.target.getAttribute('class');
         p.digito = tecla.target.innerHTML;
 
@@ -33,25 +136,25 @@ let m = {
 
     },
 
-    calculadora: function(accion, digito){
-        switch(accion){
+    calculadora: function (accion, digito) {
+        switch (accion) {
 
             case 'numero':
-                
+
                 p.cantidadSignos = 0;
 
-                if (p.operaciones.innerHTML == 0){
+                if (p.operaciones.innerHTML == 0) {
                     p.operaciones.innerHTML = digito;
-                }else{
-                    if(p.resultado){
+                } else {
+                    if (p.resultado) {
                         p.resultado = false;
                         p.operaciones.innerHTML = digito;
-                        
-                    }else{
+
+                    } else {
                         p.operaciones.innerHTML += digito;
                     }
                 }
-                
+
 
                 break;
 
@@ -61,9 +164,9 @@ let m = {
 
                 if (p.cantidadSignos == 1) {
 
-                    if(p.operaciones == 0){
+                    if (p.operaciones == 0) {
                         p.operaciones.innerHTML = 0;
-                    }else {
+                    } else {
                         p.operaciones.innerHTML += digito;
                         p.cantidadDecimal = false;
                         p.resultado = false;
@@ -71,28 +174,31 @@ let m = {
                 }
 
                 break;
-            
+
             case 'decimal':
-                
-                if (!p.cantidadDecimal){
+
+                if (!p.cantidadDecimal) {
                     p.operaciones.innerHTML += digito;
                     p.cantidadDecimal = true
+
+                    p.resultado = false;
                 }
 
                 break;
 
             case 'igual':
-                
+
                 p.operaciones.innerHTML = eval(p.operaciones.innerHTML);
                 p.resultado = true
                 break;
         }
     },
 
-    borrarCalculadora: function(){
+    borrarCalculadora: function () {
         p.operaciones.innerHTML = 0
     }
 
 }
 
-m.inicio()
+m.inicio();
+m.teclado();
